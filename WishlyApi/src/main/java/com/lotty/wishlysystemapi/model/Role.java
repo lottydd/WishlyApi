2eeb58dd061f813c894caf90612d455079fd86fa
lottydd <daniiilrybak139@gmail.com>
2025-08-16 18:33:54 +0600

    Setup model layer

---
+++ WishlyApi/src/main/java/com/lotty/wishlysystemapi/model/Role.java
@@ -0,0 +1,22 @@
+package com.lotty.wishlysystemapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "roleId", nullable = false)
+    private int roleId;
+
+    @Column(name = "roleName", nullable = false, unique = true) // Роль обычно уникальна
+    private String roleName;
+}
