197db98a3a637cc27d105dee6f308168e66c148a
lottydd <daniiilrybak139@gmail.com>
2025-08-16 13:21:30 +0600

    Removed old files

--- src/main/java/com/lotty/wishlysystemapi/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lotty.wishlysystemapi.repository;
-
-import com.lotty.wishlysystemapi.entity.ItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
-}
