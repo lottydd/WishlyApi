f2c3cf9861640014761e37d533849a82940c4280
lottydd <daniiilrybak139@gmail.com>
2025-08-17 16:47:38 +0600

    Small change

--- WishlyApi/src/main/java/com/lotty/wishlysystemapi/service/WishlistService.java
+++ WishlyApi/src/main/java/com/lotty/wishlysystemapi/service/WishlistService.java
@@ -1,13 +1,47 @@
 package com.lotty.wishlysystemapi.service;
 
+import com.lotty.wishlysystemapi.dto.request.wishlist.WishlistCreateDTO;
+import com.lotty.wishlysystemapi.mapper.WishlistMapper;
 import com.lotty.wishlysystemapi.model.Item;
+import com.lotty.wishlysystemapi.model.User;
 import com.lotty.wishlysystemapi.model.Wishlist;
+import com.lotty.wishlysystemapi.repository.UserDAO;
+import com.lotty.wishlysystemapi.repository.WishlistDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+@Service
 
 public class WishlistService {
-    public Wishlist createWishlist(Wishlist wishlist) {
+
+    private static final Logger logger = LoggerFactory.getLogger(WishlistService.class);
+
+   private final WishlistMapper wishlistMapper;
+   private final UserDAO userDAO;
+    private final WishlistDAO wishlistDAO;
+
+
+    public WishlistService(WishlistMapper wishlistMapper, UserDAO userDAO, WishlistDAO wishlistDAO) {
+        this.wishlistMapper = wishlistMapper;
+        this.userDAO = userDAO;
+        this.wishlistDAO = wishlistDAO;
     }
+
+    public Wishlist createWishlist(WishlistCreateDTO dto) {
+        logger.info("Попытка создания нового вишлиста");
+        User user = userDAO.findById(dto.getUserId())
+                .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
+
+        Wishlist wishlist =  wishlistMapper.toEntity(dto);
+        wishlist.setUser(user);
+        return wishlistDAO.save(wishlist);
+    }
+    
+    public Wishlist addItem(Integer id, Integer itemId) {
+    }
+
 
     public Wishlist getWishlistById(Integer id) {
     }
@@ -24,8 +58,7 @@ public class WishlistService {
     public Wishlist removeItem(Integer id, Integer itemId) {
     }
 
-    public Wishlist addItem(Integer id, Integer itemId) {
-    }
+
 
     public List<Item> getItems(Integer id) {
     }
